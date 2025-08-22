c604dfab1a297118fdb4a1b78925a42d6b3bfb19
Luis Rodriguez gonzalez <luisrodriguezgonzalez@MacBook-Pro-de-Luis.local>
2025-08-22 11:03:38 -0400

    sin faster

--- src/scenes/faster/helpers/getPeriodo.jsx
+++
@@ -1,9 +0,0 @@
- import dayjs from 'dayjs';
-  
-  const getPeriodo=(fecha)=>{
-    return       dayjs(fecha).format("YYYY-MM-DD").toString().replace("-","").substring(0,6);
-
-  };
-
-
-  export default  getPeriodo;  
--- src/scenes/faster/helpers/getRelacion.jsx
+++
@@ -1,6 +0,0 @@
-const getRelacion=(tableData,laid)=>{
-    return tableData.find(({ id }) => id === laid);
-  };
-
-
-  export default  getRelacion;  
